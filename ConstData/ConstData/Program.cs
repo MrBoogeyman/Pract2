596a0ee8a3bcfbea709fa10d4f815f6a46da42ab
PussyAss <Deadspace20111@yandex.ru>
2018-05-18 16:07:51 +0700

    клон оригинала

---
+++ ConstData/ConstData/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class MyMathClass
+    {
+        public const double PI = 3.14;
+        public static readonly double E;
+
+        static MyMathClass()
+        {
+            E = 2.1;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with const *****\n");
+            Console.WriteLine("The value of PI is: {0}", MyMathClass.PI);
+            Console.WriteLine("The value of E is: {0}", MyMathClass.E);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ObjectInitializers/ObjectInitializers/Rectangle .cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+    class Rectangle
+    {
+        private Point topLeft = new Point();
+        private Point botomRight = new Point();
+        public Point TopLeft
+        {
+            get { return topLeft; }
+            set { topLeft = value; }
+        }
+        public Point BottomRight
+        {
+            get { return botomRight; }
+            set { botomRight = value; }
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine("[TopLeft:{0},{1} BottomRight: {2}, {3}]", topLeft.X, topLeft.Y, botomRight.X, botomRight.Y);
+        }
+    }
+}
